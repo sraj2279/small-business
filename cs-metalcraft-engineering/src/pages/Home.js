dc1d6dfd2587ad7bef02d2652e15fa7baacd6530
sraj2279 <68522239+sraj2279@users.noreply.github.com>
2024-08-26 16:20:54 +1000

    Added scaffold folders/files for entire web app. Also some basic routing

---
+++ cs-metalcraft-engineering/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <div className="home">
+
+            <p>Hi, this is some example text</p>
+
+        </div>
+    )
+}
