d24c24d40f190ef8da070d460055e9d64eb6b43b
sraj2279 <68522239+sraj2279@users.noreply.github.com>
2024-08-26 22:21:56 +1000

    added navbar functionality

--- cs-metalcraft-engineering/src/components/Navbar.js
+++ cs-metalcraft-engineering/src/components/Navbar.js
@@ -1,12 +1,28 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import '../styles/Navbar.css'
+import logo from '../assets/CS-LOGO.png'
+import title from '../assets/CS-TITLE.png'
 
 export default function Navbar() {
     return (
-        <div className="navbar">
-            <div className="links">
-                <Link to={'/'}>Home</Link>
-            </div>
-        </div>
+        <nav className='navbar'>
+
+            <Link to={'/'}>
+                <img id='logo' src={logo} alt='CS-LOGO' />
+            </Link>
+
+            <Link className='site-title' to={'/'}>
+                <img id='title' src={title} alt='CS-TITLE' />
+            </Link>
+
+
+            <Link to={'/'}>HOME</Link>
+            <Link to={'/'}>ABOUT</Link>
+            <Link to={'/'}>PRODUCTS</Link>
+            <Link to={'/'}>CLIENTS</Link>
+            <Link to={'/'}>CONTACT US</Link>
+
+        </nav>
     )
 }
