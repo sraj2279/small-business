4fedf56f4e3644ab79189bd4d6f4e426e2a6f26a
sraj2279 <68522239+sraj2279@users.noreply.github.com>
2024-08-27 15:00:35 +1000

    added contact us page boilterplate

--- cs-metalcraft-engineering/src/App.js
+++ cs-metalcraft-engineering/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
+import Contact from './pages/Contact'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer';
 
@@ -11,6 +12,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
         <Footer />
