b2000e1c6f67ff43cfee6cd4458b728fb0b690a2
sraj2279 <68522239+sraj2279@users.noreply.github.com>
2024-08-28 19:46:17 +1000

    small edits to contact and navbar

--- cs-metalcraft-engineering/src/pages/Contact.js
+++ cs-metalcraft-engineering/src/pages/Contact.js
@@ -45,7 +45,9 @@ export default function Contact() {
 
                 <div id='google-container' className='contact-item'>
                     <div className='info-title' >FIND US</div>
-                    <div id='map' styles="width: 100%"><iframe width="100%" height="600px" frameborder="0" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Prakash%20Corrugated%20Products,%20Verna+()&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.gps.ie/">gps tracker sport</a></iframe></div>
+                    <div id='map' styles="width: 100%">
+                        <iframe title='google-map' width="100%" height="600px" frameborder="0" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Prakash%20Corrugated%20Products,%20Verna+()&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.gps.ie/">gps tracker sport</a></iframe>
+                    </div>
                 </div>
 
             </div>
